e606382d9a44138dc05e7fb298262714163192b2
braiancab <bracabral43@gmail.com>
2026-04-19 12:25:44 -0300

    Permiso de post

--- src/main/java/gm/SistemaBancario/servicio/ClienteServicioImpl.java
+++ src/main/java/gm/SistemaBancario/servicio/ClienteServicioImpl.java
@@ -5,6 +5,8 @@ import gm.SistemaBancario.repositorio.ClienteRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import java.util.List;
 
@@ -14,9 +16,12 @@ public class ClienteServicioImpl implements ClienteServicio {
 
     @Autowired
     private final ClienteRepositorio clienteRepositorio;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public ClienteServicioImpl (ClienteRepositorio clienteRepositorio) {
         this.clienteRepositorio = clienteRepositorio;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+
     }
     @Override
     @Transactional(readOnly = true)
@@ -42,6 +47,9 @@ public class ClienteServicioImpl implements ClienteServicio {
 
     @Override
     public Cliente guardarCliente(Cliente cliente) {
+        // Encriptar contraseña antes de guardar
+        String passwordEncriptada = passwordEncoder.encode(cliente.getContrasena());
+        cliente.setContrasena(passwordEncriptada);
 
         return clienteRepositorio.save(cliente);
     }
