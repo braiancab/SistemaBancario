ce29553ae78cef6c447b731d89455845bc4df1a8
Benitez_Lucas <benitezlucas51@gmail.com>
2026-04-19 18:28:05 -0300

    habilitar CORS y corregir variables de login

--- src/main/java/gm/SistemaBancario/security/SecurityConfig.java
+++ src/main/java/gm/SistemaBancario/security/SecurityConfig.java
@@ -15,8 +15,14 @@ public class SecurityConfig {
     public SecurityFilterChain filtro(HttpSecurity http) throws Exception {
 
         http
+                // 1. ABRIMOS LA PUERTA AL PUERTO DE REACT (CORS)
+                .cors(Customizer.withDefaults())
+
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        // 2. DEJAMOS PASAR AL LOGIN Y AL REGISTRO SIN PEDIR CONTRASEÑA
+                        .requestMatchers("/auth/**").permitAll()
+
                         .requestMatchers(HttpMethod.POST, "/api/clientes").permitAll()
                         .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
@@ -28,5 +34,20 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        // le damos permiso a React
+        configuration.setAllowedOrigins(java.util.Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(java.util.Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
