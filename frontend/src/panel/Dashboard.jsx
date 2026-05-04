87213110751b16d9b858cdce0711399e60c7a846
braiancab <bracabral43@gmail.com>
2026-05-04 00:21:50 -0300

    Mostrar cuentas

--- frontend/src/panel/Dashboard.jsx
+++ frontend/src/panel/Dashboard.jsx
@@ -5,7 +5,7 @@ import CuentaCard from "../componentes/cuentaCard";
 
 function Dashboard() {
   const navigate = useNavigate();
-  const { cliente, cuenta } = useDashboard(navigate);
+  const { cliente, cuentas } = useDashboard(navigate);
 
   const handleCerrarSesion = () => {
     localStorage.clear();
@@ -14,15 +14,18 @@ function Dashboard() {
 
   return (
     <div className="container mt-5">
-
       <div className="d-flex justify-content-between mb-4">
         <h2>Mi Banco</h2>
       </div>
 
       <ClienteCard cliente={cliente} />
+      <div className="row">
+  {cuentas.map((cuenta) => (
+    <div className="col-md-6" key={cuenta.idCuenta}>
       <CuentaCard cuenta={cuenta} />
-
-     
+    </div>
+  ))}
+</div>
     </div>
   );
 }
