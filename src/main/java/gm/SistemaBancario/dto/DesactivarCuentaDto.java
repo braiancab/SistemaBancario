abeba48fbdaa1050c486dc025cc8baf9e4f53bce
braiancab <bracabral43@gmail.com>
2026-06-08 23:06:16 -0300

    Dto desactivar cuenta

---
+++ src/main/java/gm/SistemaBancario/dto/DesactivarCuentaDto.java
@@ -0,0 +1,14 @@
+package gm.SistemaBancario.dto;
+
+public class DesactivarCuentaDto {
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
